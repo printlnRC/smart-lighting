ad3a9b4e9b3c9092356e0c00e6cb62e90dc2619a
printlnRC <105917920+printlnRC@users.noreply.github.com>
2024-02-08 17:23:18 +0100

    Update capteur_pr.ino

--- capteur_pr.ino
+++ capteur_pr.ino
@@ -1,6 +1,23 @@
+//------------------<Librarie>--------------------------------------------------------------------------------------------
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include <ArtronShop_BH1750.h>
+
+//------------------<Librarie>--------------------------------------------------------------------------------------------
+
+//------------------<declaration>-----------------------------------------------------------------------------------------
 const int led = 4;
 const int capteur = 2;
 int val = 0;
+#define TFT_DC 9
+#define TFT_CS 10
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+ArtronShop_BH1750 bh1750(0x23, &Wire); // Non Jump ADDR: 0x23, Jump ADDR: 0x5C
+//------------------<declaration>-----------------------------------------------------------------------------------------
+
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -12,8 +29,12 @@ void loop() {
   val = digitalRead(capteur);
 
   if (val == HIGH) {
+    if (bh1750.light() < 300 ){
+      Serial.println("La LED est allumée.");
     digitalWrite(led, HIGH);
-    delay(1000);
+    delay(2000);
+    }
+    
   } else {
     digitalWrite(led, LOW);
     delay(1000);
